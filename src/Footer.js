e9741f515a4fbf2c493044a65e5d92898c31ae5b
Anjali <anjali104ald@gmail.com>
2021-03-07 14:18:53 +0530

    Added Sidebar,Body and Footer Components..!!

---
+++ src/Footer.js
@@ -0,0 +1,14 @@
+
+import React from "react";
+import "./Footer.css";
+function Footer(){
+    return(
+        <div className="footer">
+            <h1>I am a Sidebar</h1>
+        </div>
+
+    );
+
+
+}
+export default Footer;
--- src/Player.js
+++ src/Player.js
@@ -1,10 +1,20 @@
 import React from 'react'
+import "./Player.css"
+import Sidebar from './Sidebar';
+import Body from "./Body";
+import Footer from './Footer';
 
 function Player({spotify})
 {
     return (
-        <div>
-            <h1>Authentication Successful:User Logged In</h1>
+        <div className="player">
+            <div className="player_body">
+                <Sidebar />
+                <Body />
+
+            </div>
+            <Footer />
+            
 
         </div>
     )
